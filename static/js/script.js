f9326d503fcb0e5adea214bb2955a8bcb93ed921
PaulaO98 <paulaospina9818@gmail.com>
2024-08-02 18:53:19 -0500

    feat: :sparkles: add menu and others features

--- static/js/script.js
+++ static/js/script.js
@@ -5,11 +5,20 @@ const menuArrow = menu.querySelector(".menu__arrow");
 const menuTitle = menu.querySelector(".menu__title");
 const burger = document.querySelector(".burger");
 const overlay = document.querySelector(".overlay");
+const menuDrop = menuInner.querySelector(".menu__dropdown")
+const submenu = menuInner.querySelector(".submenu")
+const submenuInner = submenu.querySelectorAll(".submenu__inner")
+const header = document.getElementById('header');
+const logo = document.getElementById("logo")
+const logoWhite = document.getElementById("logo__white")
+const menuLink = document.getElementById("menu__link")
+const menuLinks = document.querySelectorAll(".menu__link");
 
 // Navbar Menu Toggle Function
 function toggleMenu() {
   menu.classList.toggle("is-active");
   overlay.classList.toggle("is-active");
+
 }
 
 // Show Mobile Submenu Function
@@ -21,6 +30,18 @@ function showSubMenu(children) {
     .textContent;
   menu.querySelector(".menu__title").textContent = menuTitle;
   menu.querySelector(".menu__header").classList.add("is-active");
+}
+
+function showSubMenuList(children) {
+  subMenu = children.querySelector(".submenu__list");
+
+  if (subMenu.classList.contains("is-active")) {
+    subMenu.classList.remove("is-active");
+  }
+  else {
+    subMenu.classList.add("is-active");
+  }
+
 }
 
 // Hide Mobile Submenu Function
@@ -43,7 +64,50 @@ function toggleSubMenu(e) {
     const children = e.target.closest(".menu__dropdown");
     showSubMenu(children);
   }
+
 }
+
+function toggleList(e) {
+  if (e.target.closest(".submenu__inner")) {
+    const children = e.target.closest(".submenu__inner");
+
+    showSubMenuList(children);
+  }
+}
+
+const handleScroll = () => {
+
+  if (window.innerWidth < 768) {
+
+    menuLinks.forEach(link => {
+      link.style.color = '#ffffff';
+    });
+
+  }
+
+  if (window.scrollY > 50) {
+
+    header.classList.add('header-white');
+    logo.classList.add('is-active');
+    logoWhite.classList.add('is-inactive');
+    menuLinks.forEach(link => {
+      link.style.color = '#2e3094';
+    });
+
+  }
+  else {
+    header.classList.remove('header-white');
+    logoWhite.classList.remove('is-inactive');
+    logo.classList.remove('is-active');
+    menuLinks.forEach(link => {
+      link.style.color = '#ffffff';
+    });
+    
+
+  }
+
+};
+
 
 // Fixed Navbar Menu on Window Resize
 window.addEventListener("resize", () => {
@@ -60,28 +124,40 @@ overlay.addEventListener("click", toggleMenu);
 menuArrow.addEventListener("click", hideSubMenu);
 menuTitle.addEventListener("click", hideSubMenu);
 menuInner.addEventListener("click", toggleSubMenu);
+menuInner.querySelectorAll(".submenu__inner").forEach(inner => inner.addEventListener("click", toggleList));
+
+// Escuchar el evento de scroll
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('resize', handleScroll);
+
+submenuInner.forEach((div, i) => {
+  div.addEventListener("click", toggleList);
+
+});
+
+// submenuInner.addEventListener("click", toggleList);
 
 
 // =======================================
-$(document).ready(function(){
+$(document).ready(function () {
   $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 4
-          }
-      }, {
-          breakpoint: 520,
-          settings: {
-              slidesToShow: 3
-          }
-      }]
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
   });
 });
